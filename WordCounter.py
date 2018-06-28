73cc16daf746bdc3876a9a530e340e0b93d948b2
jesusprietogarcia22 <jesusprietogarcia22@gmail.com>
2018-06-28 20:55:23 +0200

    Code commit #2
    Some corrections

--- WordCounter.py
+++ WordCounter.py
@@ -29,26 +29,27 @@ def char_counter():
     return chars_number
 
 
+def main():
+	parser = argparse.ArgumentParser()
 
-parser = argparse.ArgumentParser()
+	parser.add_argument("file", type=str, help="Name of the file")
+	parser.add_argument("-lc", action="store_true", help="Number of lines in the file")
+	parser.add_argument("-wc", action="store_true", help="Number of words in the file")
+	parser.add_argument("-cc", action="store_true", help="Number of characters in the file")
 
-parser.add_argument("file", type=str, help="Name of the file")
-parser.add_argument("-lc", action="store_true", help="Number of lines in the file")
-parser.add_argument("-wc", action="store_true", help="Number of words in the file")
-parser.add_argument("-cc", action="store_true", help="Number of characters in the file")
+	args = parser.parse_args()
 
-args = parser.parse_args()
-
-print("Name of the file: ",args.file)
-if args.wc:
-    print("Number of words: ", word_counter())
-elif args.cc:
-    print("Number of characters: ", char_counter())
-elif args.lc:
-    print("Number of lines: ", line_counter())
+	print("Name of the file: ",args.file)
+	if args.wc:
+		print("Number of words: ", word_counter())
+	elif args.cc:
+		print("Number of characters: ", char_counter())
+	elif args.lc:
+		print("Number of lines: ", line_counter())
 
 
-
+if __name__ == '__main__':
+	main()
 
 
 
